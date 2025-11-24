e1c22f20311f83aeb34e29595d9396e4e3f08dd7
Yeison Estiven Perea Córdoba <ye.perea09@ciaf.edu.co>
2025-11-24 15:40:06 -0500

    Update files package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/evento-innova-frontend/",
+  base: "/evento-innova-web/",
   plugins: [react()]
 })
